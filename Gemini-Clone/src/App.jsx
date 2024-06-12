82f39256735d5303a97584af357d54ebb1b5bb1d
CyberMage7 <technician9012@gmail.com>
2024-06-12 16:36:27 +0530

    Added mainbar

--- Gemini-Clone/src/App.jsx
+++ Gemini-Clone/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './components/sidebar/Sidebar'
+import Mainbar from './components/mainbar/Mainbar'
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Mainbar />
     </>
   )
 }
